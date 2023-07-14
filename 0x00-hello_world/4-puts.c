47fa190e062fc87d811f52bf733e0c4424f366d6
Gak1o <gakiotoni@gmail.com>
2023-07-14 16:11:36 -0700

    Task 4

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+
+/**
+*main - starting point of program
+*puts -prints to screen
+*return - returns 0
+*/
+int main(void)
+{
+	puts("\"programming is like building a multilingual puzzle");
+
+	return (0);
+}
